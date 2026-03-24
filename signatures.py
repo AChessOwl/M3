65beea2dce480991c4cebb4b1adc87a2c40e9d8e
AChessOwl <dishar@seas.upenn.edu>
2026-03-23 20:34:55 -0400

    Add files via upload

---
+++ signatures.py
@@ -0,0 +1,55 @@
+from web3 import Web3
+import eth_account
+from eth_account.messages import encode_defunct
+
+
+def sign(m):
+    w3 = Web3()
+
+    account_object = w3.eth.account.create()  
+    public_key     = account_object.address    
+    private_key    = account_object.key       
+
+    message        = encode_defunct(text=m)                                          
+    signed_message = w3.eth.account.sign_message(message, private_key=private_key)    
+
+    """You can save the account public/private keypair that prints in the next section
+     for use in future assignments. You will need a funded account to pay gas fees for 
+     several upcoming assignments and the first step of funding an account is having 
+     an account to send the funds too.
+    """
+    print('Account created:\n'
+          f'private key={w3.to_hex(private_key)}\naccount={public_key}\n')
+    assert isinstance(signed_message, eth_account.datastructures.SignedMessage)
+   
+    return public_key, signed_message
+
+
+def verify(m, public_key, signed_message):
+    w3 = Web3()
+
+    message         = encode_defunct(text=m)                                                        # Encode the message
+    signer          = w3.eth.account.recover_message(message, signature=signed_message.signature)  # Recover signer address
+    valid_signature = signer.lower() == public_key.lower()                                          # True if addresses match
+
+    assert isinstance(valid_signature, bool), "verify should return a boolean value"
+    return valid_signature
+
+
+if __name__ == "__main__":
+    import random
+    import string
+
+    for i in range(10):
+        m = "".join([random.choice(string.ascii_letters) for _ in range(20)])
+
+        pub_key, signature = sign(m)
+
+        # Modifies every other message so that the signature fails to verify
+        if i % 2 == 0:
+            m = m + 'a'
+
+        if verify(m, pub_key, signature):
+            print("Signed Message Verified")
+        else:
+            print("Signed Message Failed to Verify")
